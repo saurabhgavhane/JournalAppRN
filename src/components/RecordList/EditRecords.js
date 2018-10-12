ec4a49c1cc5e8a37ef5e879f3e9347171a3369cb
Saurabh Gavhane <saurabh.gavhane@smartdatainc.net>
2018-10-12 17:54:18 +0530

    RecordList

---
+++ src/components/RecordList/EditRecords.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Platform,
+  View,
+  Image,
+  TouchableOpacity,
+  Text,
+  ToastAndroid,
+  AsyncStorage,
+  ToolbarAndroid,
+  ImageBackground,
+  FlatList
+} from "react-native";
+
+class EditRecords extends Component {
+  render() {
+    return (
+      <View>
+        <Text>EditRecords</Text>
+      </View>
+    );
+  }
+}
